328424faf27190ef76b6ffde6f2bc62132ea1c03
持书仗剑 <62134580+1204005531@users.noreply.github.com>
2023-05-08 17:02:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
